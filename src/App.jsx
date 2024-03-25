f13632ca4433ef332aaefb509da2acb2734c8298
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-25 20:59:04 +0100

    finish props for all components

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ const plaviStil = {
   fontWeight: "bold",
 };
 
-const urlSlike =
-  "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg";
-
 function GreenTitle(props) {
-
-  console.log("green title props", props)
   return (
     <div className={props.className}>
       <h1
@@ -25,12 +20,21 @@ function GreenTitle(props) {
   );
 }
 
-function BlueSubtitle() {
-  return <h2 style={plaviStil}>Lorem ipsum dolor sit amet consectetur</h2>;
+function BlueSubtitle(props) {
+  const { title = "plavi subtitle", className } = props;
+
+  return (
+    <h2 className={className} style={plaviStil}>
+      {title}
+    </h2>
+  );
 }
 
-function CatImage() {
-  return <img src={urlSlike} alt="Cat" title="Mačak" width="200" />;
+const defaultCatImageUrl =
+  "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg";
+
+function CatImage({ src = defaultCatImageUrl, className, width=200 }) {
+  return <img className={className} src={src} alt="Cat" title="Mačak" width={width+"px"} />;
 }
 
 function Calculation(props) {
@@ -49,14 +53,22 @@ function Calculation(props) {
 function App() {
   return (
     <div>
-      <GreenTitle 
-        title="Naslov iz App komponente" 
+      <GreenTitle
+        title="Naslov iz App komponente"
         className="nekaKlasa zeleni tekst"
       />
       <br />
+      <BlueSubtitle title="I'm blue dabudedadbabdab" />
       <BlueSubtitle />
 
-      <CatImage />
+      <CatImage className="maca1" />
+
+      <CatImage
+        className="maca2 narancasta"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRv4XycGdx5_m1P2W0bMG1xqcpA7kucbaULP8Kx2TGBQ&s"
+        width={400}
+
+      />
 
       <Calculation x={42} y={18} />
       <Calculation x={15} y={21} />
