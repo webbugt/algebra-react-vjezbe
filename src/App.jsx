e96ae36bb657e142289e3fb8ca9b3ed6938c00b3
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-25 20:36:35 +0100

    lint and add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,68 @@
-
 const plaviStil = {
   color: "blue",
   textDecoration: "underline",
-  fontWeight: "bold"
+  fontWeight: "bold",
+};
+
+const urlSlike =
+  "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg";
+
+function GreenTitle(props) {
+
+  console.log("green title props", props)
+  return (
+    <div className={props.className}>
+      <h1
+        style={{
+          ...plaviStil,
+          color: "green",
+          background: "yellow",
+        }}
+      >
+        {props.title}
+      </h1>
+    </div>
+  );
 }
 
-const urlSlike = "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
-
-function GreenTitle(){
-  return <h1 style={
-      {
-        ...plaviStil,
-        color: "green",
-        background: "yellow"
-      }
-    }>
-      Lorem ipsum dolor
-    </h1>
+function BlueSubtitle() {
+  return <h2 style={plaviStil}>Lorem ipsum dolor sit amet consectetur</h2>;
 }
 
-function BlueSubtitle(){
-  return <h2 style={plaviStil}>
-      Lorem ipsum dolor sit amet consectetur
-    </h2>
+function CatImage() {
+  return <img src={urlSlike} alt="Cat" title="Mačak" width="200" />;
 }
 
-function CatImage(){
-  return <img 
-      src={urlSlike} 
-      alt="Cat" 
-      title="Mačak" 
-      width="200"
-    />
-}
-
-
-function Calculation(){
-  
+function Calculation(props) {
   // brojevi za zbrojiti
-  const x = 5
-  const y = 10
+  const x = props.x;
+  const y = props.y;
 
-  return (<p>
-      x + y = {x} + {y} = {x + y}
-    </p>)
+  return (
+    <p>
+      a + b = {x} + {y} = {x + y}
+    </p>
+  );
 }
 
 // funkcionalna komponenta
 function App() {
+  return (
+    <div>
+      <GreenTitle 
+        title="Naslov iz App komponente" 
+        className="nekaKlasa zeleni tekst"
+      />
+      <br />
+      <BlueSubtitle />
 
-  return <div>
-    
-    <GreenTitle title="Hello world" />
-    <CatImage />
+      <CatImage />
 
-    <BlueSubtitle />
-    <GreenTitle title="Hello world" />
-    
-    <Calculation />
-    <GreenTitle title="Hello world" />
-
-  </div>
+      <Calculation x={42} y={18} />
+      <Calculation x={15} y={21} />
+      <Calculation x={3} y={-14} />
+    </div>
+  );
 }
 
-export default App
+export default App;
