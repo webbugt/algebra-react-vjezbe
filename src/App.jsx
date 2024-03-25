a66f4d21805bd02bb800fae674b9057aa9863191
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-25 20:14:03 +0100

    isolated app components

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,59 @@ const plaviStil = {
 
 const urlSlike = "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
 
-// funkcionalna komponenta
-function App() {
-
-  // brojevi za zbrojiti
-  const x = 5
-  const y = 10
-
-  return <div>
-    <h1 style={
+function GreenTitle(){
+  return <h1 style={
       {
         ...plaviStil,
         color: "green",
         background: "yellow"
       }
-    }>Lorem ipsum dolor</h1>
+    }>
+      Lorem ipsum dolor
+    </h1>
+}
 
-    <h2 style={plaviStil}>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <img 
+function BlueSubtitle(){
+  return <h2 style={plaviStil}>
+      Lorem ipsum dolor sit amet consectetur
+    </h2>
+}
+
+function CatImage(){
+  return <img 
       src={urlSlike} 
       alt="Cat" 
       title="Mačak" 
       width="200"
     />
+}
 
-    <p>
+
+function Calculation(){
+  
+  // brojevi za zbrojiti
+  const x = 5
+  const y = 10
+
+  return (<p>
       x + y = {x} + {y} = {x + y}
-    </p>
+    </p>)
+}
+
+// funkcionalna komponenta
+function App() {
+
+  return <div>
+    
+    <GreenTitle title="Hello world" />
+    <CatImage />
+
+    <BlueSubtitle />
+    <GreenTitle title="Hello world" />
+    
+    <Calculation />
+    <GreenTitle title="Hello world" />
+
   </div>
 }
 
