cb915d5c1380095040fcb3d8cb8dbb82e8142651
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-27 20:51:08 +0100

    impelment green title for kontakt

--- src/routes/Kontakt.jsx
+++ src/routes/Kontakt.jsx
@@ -1,11 +1,12 @@
 
 import Header from "../components/Header.jsx";
+import { GreenTitle } from "../components/Titles.jsx";
 
 export function Kontakt() {
   return (
     <div>
       <Header />
-      <h1>Kontakt</h1>
+      <GreenTitle title="Kontakt" />
     </div>
   );
 }
