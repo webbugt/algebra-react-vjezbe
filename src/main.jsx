9b2f66323634ec592c462cd2438312d7f122047e
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-27 19:54:32 +0100

    basic react-router with anchors

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/onama",
+    element: <h1>O nama</h1>
+  },
+  {
+    path: "/kontakt",
+    element: <h1>Kontakt</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <header>
+      <a href="/">Naslovna</a>
+      <a href="/onama">O nama</a>
+      <a href="/kontakt">Kontakt</a>
+    </header>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
