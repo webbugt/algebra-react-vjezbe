b7d4d05a3386f2adcd44e01b6b23e6c3af1c9f3e
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-10 19:26:21 +0200

    initial state inside component

--- src/components/KomponentaSaStanjem.jsx
+++ src/components/KomponentaSaStanjem.jsx
@@ -1,10 +1,13 @@
 import propTypes from 'prop-types'
+import {useState} from 'react'
 
 function KomponentaSaStanjem({ nekiNaslov }){
-    
+    const [count] = useState(10)
 
     return <span>
         {nekiNaslov}
+        <h2>Trenutno stanje: {count}</h2>
+        {/* <h2>{staticnoStanje.count}</h2> */}
     </span>
 }
 
