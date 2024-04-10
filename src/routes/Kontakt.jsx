153b2e0c271d393d0e449bdbf28436beb5d78e51
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-10 21:07:43 +0200

    use onChange prop in Kontakt

--- src/routes/Kontakt.jsx
+++ src/routes/Kontakt.jsx
@@ -4,12 +4,17 @@ import Header from "../components/Header.jsx";
 import { GreenTitle } from "../components/Titles.jsx";
 
 export function Kontakt() {
+
+  const buttonChangeHandler = (eventName) => {
+    console.log("button triggered an event "+eventName)
+  }
+
   return (
     <div>
       <span>neka izmjena</span>
       <Header />
       <GreenTitle title="Kontakt" />
-      <EventButton />
+      <EventButton onChange={buttonChangeHandler}/>
     </div>
   );
 }
