f253fd2ee56958f2ecd9a5e413d2a13b0ed257ae
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-10 18:17:38 +0200

    izmjena

--- src/routes/Kontakt.jsx
+++ src/routes/Kontakt.jsx
@@ -5,6 +5,7 @@ import { GreenTitle } from "../components/Titles.jsx";
 export function Kontakt() {
   return (
     <div>
+      <span>neka izmjena</span>
       <Header />
       <GreenTitle title="Kontakt" />
     </div>
