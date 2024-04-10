22ad6d16417591cafaaf89ab21b3667d11934656
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-10 21:12:09 +0200

    connect state to onChange in EventButton

--- src/routes/Kontakt.jsx
+++ src/routes/Kontakt.jsx
@@ -1,19 +1,22 @@
 
+import { useState } from "react";
 import EventButton from "../components/EventButton.jsx";
 import Header from "../components/Header.jsx";
 import { GreenTitle } from "../components/Titles.jsx";
 
 export function Kontakt() {
 
+  const [lastState,setLastState] = useState("none")
+
   const buttonChangeHandler = (eventName) => {
-    console.log("button triggered an event "+eventName)
+    // console.log("button triggered an event "+eventName)
+    setLastState("event: "+eventName)
   }
 
   return (
     <div>
-      <span>neka izmjena</span>
       <Header />
-      <GreenTitle title="Kontakt" />
+      <GreenTitle title={lastState} />
       <EventButton onChange={buttonChangeHandler}/>
     </div>
   );
