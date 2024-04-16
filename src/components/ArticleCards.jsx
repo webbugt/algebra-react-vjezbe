801de6cea9c96d539477eb7cacfbaf4e8f099e1d
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-16 20:47:28 +0200

    dodavanje slike u karticu

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -1,3 +1,5 @@
+import propTypes from "prop-types";
+
 const articles = [
   {
     id: 18,
@@ -38,17 +40,16 @@ const articles = [
     image: "https://via.assets.so/album.png?id=4&q=95&w=130&h=130&fit=fill",
   },
   {
-    id: 24,
+    id: 23,
     title: "Futurizam u Arhitekturi",
     description:
       "Istražujemo kako futurizam oblikuje moderne arhitektonske pothvate",
     slug: "futurizam-u-arhitekturi-90op0p",
+    image: "https://via.assets.so/album.png?id=7&q=95&w=130&h=130&fit=fill"
   },
 ];
 
-
 // const articleMapperArrow = ({id, title}) => <ArticleCard key={id} title={title} />
-
 
 // function articleMapperFunction(article){
 //     const {id, title} = article
@@ -56,19 +57,39 @@ const articles = [
 //     return <ArticleCard key={id} title={title} />
 // }
 
-function ArticleCard({title}){
-    return <div className="ArticleCard">
-        <h2>{title}</h2>
+function ArticleCard({ title, imageUrl }) {
+    if(!title || !imageUrl){
+        return null
+    }
+  return (
+    <div className="ArticleCard" style={{
+        display:"flex",
+        gap: 20,
+        margin: 10
+    }}>
+        <img src={imageUrl} className="ArticleCard_image" title={title} alt={title} width="130" height="130" />
+      <h2>{title}</h2>
     </div>
+  );
 }
 
+ArticleCard.propTypes = {
+  title: propTypes.string.isRequired,
+  imageUrl: propTypes.string.isRequired,
+};
+
 function ArticleCards() {
-    return <div className="ArticleCards">
-        {/* {articles.map(({id, title}) => <ArticleCard key={id} title={title} />)} */}
-        {articles.map(({id, title}) => {
-            return <ArticleCard key={id} title={title} />
-        })}
+  return (
+    <div className="ArticleCards">
+      {articles.map(({ id, title, image }) => {
+        return <ArticleCard 
+            key={id} 
+            title={title} 
+            imageUrl={image}
+        />;
+      })}
     </div>
+  );
 }
 
 export default ArticleCards;
