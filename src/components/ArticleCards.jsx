1d3251ce1b6e0afda9380b1cfd4467ac242952db
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-16 20:17:32 +0200

    add chatGpt generated content

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -1,18 +1,44 @@
-
 const articles = [
-    {
-        id: 18,
-        title: "Neki naslov članka",
-        description: "Neki opis članka malo dulje ide ovdje",
-        slug: "neki-naslov-clanka-12ge4r",
-        image: ""
-    }
-]
+  {
+    id: 18,
+    title: "Neki naslov članka",
+    description: "Neki opis članka malo dulje ide ovdje",
+    slug: "neki-naslov-clanka-12ge4r",
+    image: "https://via.assets.so/album.png?id=1&q=95&w=130&h=130&fit=fill",
+  },
+  {
+    id: 19,
+    title: "Istraživanje Svemira i Nove Tehnologije",
+    description:
+      "Otkrivamo kako nove tehnologije transformiraju naše istraživanje svemira",
+    slug: "istrazivanje-svemira-i-nove-tehnologije-45rt56",
+    image: "https://via.assets.so/album.png?id=2&q=95&w=130&h=130&fit=fill",
+  },
+  {
+    id: 20,
+    title: "Zaštita Okoliša i Održivi Razvoj",
+    description:
+      "Pregled najnovijih inicijativa za očuvanje okoliša i poticanje održivog razvoja",
+    slug: "zastita-okolisa-i-odrzivi-razvoj-78ui89",
+    image: "https://via.assets.so/album.png?id=3&q=95&w=130&h=130&fit=fill",
+  },
+  {
+    id: 21,
+    title: "Futurizam u Arhitekturi",
+    description:
+      "Istražujemo kako futurizam oblikuje moderne arhitektonske pothvate",
+    slug: "futurizam-u-arhitekturi-90op0p",
+  },
+  {
+    id: 22,
+    title: "Umjetna Inteligencija u Medicini",
+    description:
+      "Dubinski pogled na ulogu umjetne inteligencije u revolucioniranju zdravstvene skrbi",
+    slug: "umjetna-inteligencija-u-medicini-23gh67",
+    image: "https://via.assets.so/album.png?id=4&q=95&w=130&h=130&fit=fill",
+  },
+];
 
-function ArticleCards(){
+function ArticleCards() {}
 
-}
-
-
-
-export default ArticleCards
+export default ArticleCards;
