9ad65f0df04b6f5b5a258d86136cca969613b750
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-16 20:13:25 +0200

    clanci pocetno

---
+++ src/components/ArticleCards.jsx
@@ -0,0 +1,18 @@
+
+const articles = [
+    {
+        id: 18,
+        title: "Neki naslov članka",
+        description: "Neki opis članka malo dulje ide ovdje",
+        slug: "neki-naslov-clanka-12ge4r",
+        image: ""
+    }
+]
+
+function ArticleCards(){
+
+}
+
+
+
+export default ArticleCards
