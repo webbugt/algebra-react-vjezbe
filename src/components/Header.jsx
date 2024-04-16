4a83424e7a1d29f723f632105ec69bee9f589bb2
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-16 17:57:35 +0200

    fix: style header a bit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,34 @@
 // import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <header style={{
-    fontSize: 25,
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    display: "flex",
-    gap: 14,
-    justifyContent: "center",
-    padding: 10,
-  }}>
-    <Link to="/">Naslovna</Link>
-    <Link to="/onama">O nama</Link>
-    <Link to="/kontakt">Kontakt</Link>
-  </header>;
+  return (
+    <>
+      <div
+        style={{
+          marginBottom: 80,
+        }}
+      />
+      <header
+        style={{
+          fontSize: 25,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          display: "flex",
+          gap: 14,
+          justifyContent: "center",
+          padding: 10,
+          background: "white"
+        }}
+      >
+        <Link to="/">Naslovna</Link>
+        <Link to="/onama">O nama</Link>
+        <Link to="/kontakt">Kontakt</Link>
+      </header>
+    </>
+  );
 }
 
-
-export default Header
+export default Header;
