1fa2ac8f5c8d426d0e6b5edbd622abae17ae12d7
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-29 20:59:36 +0200

    prop drill pain example

--- src/components/PropDrillExample.jsx
+++ src/components/PropDrillExample.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
+import propTypes from 'prop-types'
 
-function PropDrillKomponenta4({ naslov = "default" }) {
+function PropDrillKomponenta4({ naslov }) {
   return <p> Pozdrav iz komponente 4 {naslov}</p>;
 }
-function PropDrillKomponenta3({ tekst }) {
-  return <PropDrillKomponenta4 naslov={tekst} />;
+PropDrillKomponenta4.propTypes = {
+  naslov: propTypes.string
 }
+PropDrillKomponenta4.defaultProps = {
+  naslov: "default new"
+}
+
+function PropDrillKomponenta3({ tekst }) {
+  return <PropDrillKomponenta4 naslov={tekst}  />;
+}
+PropDrillKomponenta3.propTypes = {
+  tekst: propTypes.string
+}
+PropDrillKomponenta3.defaultProps = {
+  tekst:"default from 3"
+}
+
 function PropDrillKomponenta2({ title }) {
   return <PropDrillKomponenta3 tekst={title} />;
 }
+PropDrillKomponenta2.propTypes = {
+  title: propTypes.string
+}
+PropDrillKomponenta2.defaultProps = {
+  title:"default from 2"
+}
+
 export function PropDrillKomponenta1() {
   const [name, setName] = useState("perica");
   return <div>
