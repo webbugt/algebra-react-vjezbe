aa89b61c5059dcb16fdde916edda28cbab641ddd
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-29 19:02:16 +0200

    connect state to input component

--- src/components/forme/FunctionalNameForm.jsx
+++ src/components/forme/FunctionalNameForm.jsx
@@ -3,14 +3,16 @@ import { useState } from "react";
 export function FunctionalNameForm() {
   const [name, setName] = useState("Pero");
   const onInputChangeHandler = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setName(event.target.value)
   };
   return (
     <form>
       <label htmlFor="name">Ime</label>
       <input
         name="name"
-        type="text" //value={name}
+        type="text" 
+        value={name}
         onChange={onInputChangeHandler}
       />
       <button type="submit">Pošalji</button>
