b2861f1445d3fbafcfe49dcaf7e5b195642b2c2f
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-04-29 18:59:45 +0200

    basic onInputChangeHandler

--- src/components/forme/FunctionalNameForm.jsx
+++ src/components/forme/FunctionalNameForm.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export function FunctionalNameForm() {
+  const [name, setName] = useState("Pero");
+  const onInputChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <form>
       <label htmlFor="name">Ime</label>
-      <input name="name" type="text" />
+      <input
+        name="name"
+        type="text" //value={name}
+        onChange={onInputChangeHandler}
+      />
       <button type="submit">Pošalji</button>
+      <pre>Trenutna vrijednost: {name}</pre>
     </form>
   );
 }
