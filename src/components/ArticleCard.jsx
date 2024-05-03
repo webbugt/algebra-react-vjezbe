44eeb6983035c53b133f750e2de4ec3a4f8f1008
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-03 20:32:43 +0200

    article card styles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,52 +1,35 @@
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
-import style from "./ArticleCard.module.css"
+import style from "./ArticleCard.module.css";
 
 export function ArticleCard({ title, imageUrl, href, description }) {
-    if (!title || !imageUrl || !href) {
-        return null;
-    }
-    return (
-        <Link
-            to={href}
-            className={style.card}
-            style={{
-                display: "flex",
-                gap: 20,
-                margin: 10,
-                textDecoration: "none",
-                color: "white",
-            }}
-        >
-            <img
-                src={imageUrl}
-                className="ArticleCard_image"
-                title={title}
-                alt={title}
-                width="130"
-                height="130" />
-            <div
-                className="ArticleCard_content"
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                }}
-            >
-                <h2 className={style.title}>{title}</h2>
-                {!!description && (
-                    <p className="ArticleCard_description">{description}</p>
-                )}
-            </div>
-        </Link>
-    );
+  if (!title || !imageUrl || !href) {
+    return null;
+  }
+  return (
+    <Link to={href} className={style.card}>
+      <img
+        src={imageUrl}
+        title={title}
+        alt={title}
+        width="130"
+        height="130"
+      />
+      <div className={style.content}>
+        <h2 className={style.title}>{title}</h2>
+        {!!description && (
+          <p>{description}</p>
+        )}
+      </div>
+    </Link>
+  );
 }
 ArticleCard.propTypes = {
-    title: propTypes.string.isRequired,
-    imageUrl: propTypes.string.isRequired,
-    href: propTypes.string.isRequired,
-    description: propTypes.string,
+  title: propTypes.string.isRequired,
+  imageUrl: propTypes.string.isRequired,
+  href: propTypes.string.isRequired,
+  description: propTypes.string,
 };
 ArticleCard.defaultProps = {
-    description: undefined,
+  description: undefined,
 };
