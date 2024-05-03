5621ed239a757eb959f6ad6cadd403064e4f8a34
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-03 20:06:24 +0200

    card global css example

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,6 @@
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
+import "../card.css"
 
 export function ArticleCard({ title, imageUrl, href, description }) {
     if (!title || !imageUrl || !href) {
