4fe51bc1ac900d48f60fd6d04d11ebea622fca70
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-03 21:01:59 +0200

    use in some example titles

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,18 @@ function App() {
       <Header />
       <GreenTitle title="Vrijednost iz parent komponente"/>
       <br />
-      <GreenSubtitle title="I'm blue dabudedadbabdab" />
+      <GreenSubtitle
+        title="I'm blue dabudedadbabdab" 
+        className="text-slate-900"
+      />
       <BlueSubtitle title="mora biti prisutan" />
       <MagentaSubtitle title="kupi mobitel" />
       <RedSubtitle title="ferrari" />
 
       <CatImage className="maca1" />
-
+{/* Tailwind example */}
+      <p className="text-sm leading-6 font-semibold text-slate-500 my-[200px]">OpenAi/subtitle</p>
+      <h1 className="mt-4 max-w-5xl text-slate-900 text-3xl sm:text-6xl tracking-tight font-bold dark:text-white">Naslov ko OpenAI</h1>
       <CatImage
         className="maca2 narancasta"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRv4XycGdx5_m1P2W0bMG1xqcpA7kucbaULP8Kx2TGBQ&s"
