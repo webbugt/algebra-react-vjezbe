4e925bee8b6d1c18d3cff9f6db7a2a5ff4965ffb
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-03 18:54:50 +0200

    implement withColor color property

--- src/hoc/withColor.jsx
+++ src/hoc/withColor.jsx
@@ -1,8 +1,8 @@
 
 
-const withColor = (Component) => {
+const withColor = (Component,color) => {
     const ComponentWithColor = ({...rest}) => {
-        return <Component {...rest}  color="magenta"/>
+        return <Component {...rest}  color={color}/>
     }
     return ComponentWithColor
 }
