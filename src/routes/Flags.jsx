9266a8387a55d05480ed3b2d0f84e8f67179b192
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-07 20:01:10 +0200

    basic api get

--- src/routes/Flags.jsx
+++ src/routes/Flags.jsx
@@ -1,12 +1,30 @@
 
+import axios from "axios";
 import Header from "../components/layout/Header.jsx";
+import { useEffect, useState } from "react";
 
+
+function FlagsOfTheWorld () {
+  const [countries,setCountries] = useState(null)
+
+  useEffect(()=>{
+    axios.get("https://restcountries.com/v2/all")
+    .then(({data})=>setCountries(data))
+  },[])
+
+  if(countries === null){
+    return "Loading"
+  }
+
+  return "Flags"
+}
 
 export function FlagsPage() {
   return (
     <div>
       <Header />
       <h1>zastave svijeta</h1>
+      <FlagsOfTheWorld />
     </div>
   );
 }
