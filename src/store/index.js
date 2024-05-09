7d15e0b12884e4127a89ad49f6214af5ea345ceb
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-09 20:52:32 +0200

    create and connect saga middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import counterReducer from './counterSlice'
 
+import createSagaMiddleware from '@redux-saga/core'
+
+const sagaMiddleware = createSagaMiddleware()
+
 export const store = configureStore({
     reducer: {
         counter: counterReducer
-    }
+    },
+    middleware: [sagaMiddleware]
 })
