4cee5e867246e7f3c48396e8f306f94ed31ba8c5
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-09 19:27:32 +0200

    finish Counter example

--- src/containers/Counter.jsx
+++ src/containers/Counter.jsx
@@ -1,11 +1,21 @@
 import propTypes from "prop-types";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../store/counterSlice";
 
-const COUNTER_KEY = "counter-value";
+// const COUNTER_KEY = "counter-value";
 
 const countSelector = (state) => state.counter.value;
+// eslint-disable-next-line no-unused-vars
 export function Counter({ initial }) {
   const count = useSelector(countSelector);
+  const dispatch = useDispatch();
+
+  const addOne = () => {
+    dispatch(increment())
+  }
+  const reduceOne = () => {
+    dispatch(decrement())
+  }
 
   return (
     <div>
@@ -13,8 +23,8 @@ export function Counter({ initial }) {
         Stanje brojača:{" "}
         <h3 style={{ fontSize: 40, weight: "bold" }}>{count}</h3>
       </h2>
-      <button>Dodaj 1</button>
-      <button>Oduzmi 1</button>
+      <button onClick={addOne}>Dodaj 1</button>
+      <button onClick={reduceOne}>Oduzmi 1</button>
     </div>
   );
 }
