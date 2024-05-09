f678670d1adeaff224948682de75eb6b78a1318f
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-09 19:30:36 +0200

    example of anotehr component that consumes same state

--- src/routes/Onama.jsx
+++ src/routes/Onama.jsx
@@ -1,6 +1,16 @@
 import Header from "../components/layout/Header.jsx";
 import DashboardParent from "../containers/ParentBezStanja.jsx";
 import { FancyTitle } from "../components/Titles.jsx";
+import { useSelector } from "react-redux";
+
+
+const countSelector = (state) => state.counter.value;
+function ShowCount (){
+  const count = useSelector(countSelector)
+
+  return <b style={{fontSize: 80}}>{count}</b>
+}
+
 
 export function Onama() {
   return (
@@ -10,6 +20,8 @@ export function Onama() {
         title="O nama" 
         subtitle="Hello" 
       />
+      <ShowCount />
+      <br/>
 
       <DashboardParent id="prva komponenta" nekiNaslov="Neki drugi naslov za Stanje" initialCount={20} />
       <DashboardParent id="matija" nekiNaslov="Naslov iz O nama" initialCount={-50} />
