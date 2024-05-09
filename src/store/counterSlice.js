c01e7794573e90342ac85192a3a5209024953fe4
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-09 19:08:12 +0200

    counterSlice created and reducer connected

---
+++ src/store/counterSlice.js
@@ -0,0 +1,28 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    value: 0,
+}
+
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment: (currentState) => {
+            return {
+                ...currentState,
+                value: currentState.value + 1
+            }
+        },
+        decrement: (currentState) => {
+            return {
+                ...currentState,
+                value: currentState.value - 1
+            }
+        },
+    }
+})
+
+export const {increment, decrement} = counterSlice.actions
+
+export default counterSlice.reducer
